08f65f98cd1eb2ba84ba587450d25e050d5878a4
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2024-11-18 19:15:13 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: #f8f9fa;
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <div></div>
+    </Wrapper>
+  );
+}
+
+export default App;
