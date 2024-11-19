29a18ba73d58a03cc74b1733757e30445f57c94d
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2024-11-19 14:06:01 -0300

    wrap App component with ThemeProvider and add GlobalStyle for improved theming support

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
